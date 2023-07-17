f99bdba681b1742f6f08c26cfe1108d795390353
Kachi <onlinehassle1234@gmail.com>
2023-07-17 17:24:39 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Logo from "../assets/img/logo.svg";
+
+import { footer } from "../data";
+
+const Footer = () => {
+  return (
+    <footer className="section bg-primary text-white">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between border-b border-opacity-75 border-gray-700 pb-7 lg:pb-14 mb-14 ">
+          <a className="mb-6 lg:mb-0" href="#">
+            <img src={Logo} alt="" />
+          </a>
+          {/* social */}
+          <div className="flex gap-x-4 ">
+            {footer.social.map((item, index) => {
+              return (
+                <div
+                  className="w-12 h-12 text-2xl bg-gray-700 hover:bg-accent rounded-full flex justify-center items-center transition"
+                  key={index}
+                >
+                  <a href="#">{item.icon}</a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {/* copyright */}
+        <p className="text-center">
+          &copy; FurniShop 2023 - All right reserved
+          <span className="text-[20px] text-orange-600"> KACHI</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
